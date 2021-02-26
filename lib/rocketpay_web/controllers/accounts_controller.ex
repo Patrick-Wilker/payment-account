397675a77368f1e2cbe0622b037785ca2667dee7
Patrick <patrickvieiramota@gmail.com>
2021-02-25 21:24:41 -0300

    End deposit function

--- lib/rocketpay_web/controllers/accounts_controller.ex
+++ lib/rocketpay_web/controllers/accounts_controller.ex
@@ -8,8 +8,8 @@ defmodule RocketpayWeb.AccountsController do
   def deposit(conn, params) do
     with {:ok, %Account{} = account} <- Rocketpay.deposit(params) do
       conn
-      |> put_status(:created)
-      |> render("update.json", Account: account)
+      |> put_status(:ok)
+      |> render("update.json", account: account)
     end
   end
 end
